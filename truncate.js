a4b9f2f364f50f09c10886bcadf014cc6212b861
Sam Egger <segger87@gmail.com>
2018-02-05 20:41:13 +0000

    Challenge 9 - Truncate a string

---
+++ truncate.js
@@ -0,0 +1,19 @@
+/* Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+Note that inserting the three dots to the end will add to the string length.
+However, if the given maximum string length num is less than or equal to 3, then the addition of the three dots does not add to the string length in determining the truncated string. */
+
+
+//using .slice() and placing a 0 as the first integer ensures that the value from num is taken off the end of the string
+
+function truncateString(str, num) {
+  var stringDot = '...';
+  if (num <= 3) {
+    return str.slice(0, num) + stringDot;
+  } else if (str.length > num) {
+    return str.slice(0, (num -3)) + stringDot;
+  } else {
+    return str;
+  }
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 200);
