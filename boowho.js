075dfb2c1b740536ba553adcb4df3f92d4909569
Sam Egger <segger87@gmail.com>
2018-02-26 20:55:24 +0000

    INT - Challenge 9 - Boo Who

---
+++ boowho.js
@@ -0,0 +1,8 @@
+//Check if a value is classified as a boolean primitive. Return true or false.
+
+//typeof returns what type of input something is (string, integer, boolean )
+function booWho(bool) {
+  return typeof bool === 'boolean';
+}
+
+booWho(null);
