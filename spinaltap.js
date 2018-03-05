ba304a97e05a8fce81abc39a76b974944e59c9a5
Sam Egger <segger87@gmail.com>
2018-03-05 21:19:59 +0000

    INT Challen 2 - Spinal Tap

---
+++ spinaltap.js
@@ -0,0 +1,10 @@
+//Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+function spinalCase(str) {
+  //adds space between capital letters
+  var addSpace = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+  //replaces whitespace and underscores with -
+  return addSpace.replace(/( )|(_)/gi, "-").toLowerCase();
+}
+
+spinalCase("The_Andy_Griffith_Show");
